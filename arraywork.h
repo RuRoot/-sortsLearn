05e048219acef41bde7eea001fe9cb03b7ba35e2
RuRoot <122613708+RuRoot@users.noreply.github.com>
2023-02-16 20:32:06 +0300

    Update arraywork.h

--- arraywork.h
+++ arraywork.h
@@ -1,1 +1,11 @@
+#include <iostream>
+using namespace std;
+#ifndef arraywork.h
+#define arraywork.h
 
+int isSort (int* ar,int size);
+void randomFilling(int* ar,int size,int min,int max);
+void print(int* ar,int size);
+int retMax(int* ar,int size);
+int retMin(int* ar,int size);
+#endif
